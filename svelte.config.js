58d0c73dfa747d4cbdf71020139b8b3ee9d51c9c
Paolo Innocenti <paolo.innocenti@laserradesca.com>
2024-02-14 17:54:39 +0100

    deploy2

--- svelte.config.js
+++ svelte.config.js
@@ -15,7 +15,10 @@ const config = {
 			fallback: undefined,
 			precompress: false,
 			strict: true
-		})
+		}),
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		}
 	},
 	preprocess: vitePreprocess()
 };
